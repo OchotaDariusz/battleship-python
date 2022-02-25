49b51f84ae36fa447972952bae77fa22e4e98f53
Dariusz Ochota <ochota.dariusz@gmail.com>
2022-02-25 21:35:03 +0100

    Create setup_ships() function

--- battleship.py
+++ battleship.py
@@ -11,6 +11,9 @@ def main():
     player_1_board, player_2_board = utils.setup_game()
     console_clear()
     display.print_board(player_1_board, player_2_board)
+    player_1_ships, player_2_ships = utils.setup_ships()
+    print(player_1_ships)
+    print(player_2_ships)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -32,4 +32,32 @@ def build_board(height: int, width: int, board: list) -> list:
 def setup_game():
     height, width = get_size_from_input()
     player_1_board, player_2_board = generate_boards(height, width)
-    return player_1_board,player_2_board
+    return player_1_board, player_2_board
+
+
+def setup_ships() -> list:
+    ships = 3
+    player_1_ships, player_2_ships = list(), list()
+    setup_complete = False
+    players_done_setup = 0
+    while setup_complete is not True:
+        print("Player 1 turn")
+        for i in range(ships):
+            player_setup(player_1_ships)
+        players_done_setup += 1
+        print("Player 2 turn")
+        player_setup(player_2_ships)
+        players_done_setup += 1
+        if players_done_setup == 2:
+            setup_complete = True
+    return player_1_ships, player_2_ships
+
+
+def player_setup(ships: list):
+    inputs = list()
+    user_input = input("Select field (ex. B2)").upper()
+    row = int(ord(user_input[:1])) - 65
+    col = int(user_input[1:]) - 1
+    inputs.append(row)
+    inputs.append(col)
+    ships.append(inputs)
