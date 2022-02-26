f4e2860b2386a5bf6a2d2306e4fbc2aa4c0ae3bd
Dariusz Ochota <ochota.dariusz@gmail.com>
2022-02-26 14:07:03 +0100

    Added shooting_phase(...) func

--- battleship.py
+++ battleship.py
@@ -12,9 +12,48 @@ def main():
     print("Player 1 ships:", player_1_ships)
     print("Player 1 disallowed fields:", p1_disallowed_fields)
     print("Player 1 hitboard:", player_1_hitboard)
+
     print("Player 2 ships:", player_2_ships)
     print("Player 2 disallowed fields:", p2_disallowed_fields)
     print("Player 2 hitboard:", player_2_hitboard)
+
+    while len(player_1_hitboard) != 0 or len(player_2_hitboard) != 0:
+        print("Player 2 hitboard:", player_2_hitboard)
+        print("Player 1 shooting phase!")
+        shooting_phase(player_2_hitboard)
+
+        if len(player_2_hitboard) == 0:
+            print("Player 1 wins!")
+            break
+
+        print("Player 1 hitboard:", player_1_hitboard)
+        print("Player 2 shooting phase!")
+        shooting_phase(player_1_hitboard)
+
+        if len(player_1_hitboard) == 0:
+            print("Player 2 wins!")
+            break
+
+
+def shooting_phase(player_hitboard: list):
+    hit_combo = True
+    while hit_combo is True:
+        if len(player_hitboard) == 0:
+            break
+        inputs = list()
+        row, col = utils.get_input()
+        inputs.append(row)
+        inputs.append(col)
+        if inputs in player_hitboard:
+            print("You've hit a ship!")
+            for index in range(len(player_hitboard)):
+                if player_hitboard[index] == inputs:
+                    player_hitboard.pop(index)
+                    break
+            hit_combo = True
+        else:
+            print("You've missed!")
+            hit_combo = False
 
 
 if __name__ == "__main__":
